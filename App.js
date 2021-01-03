4cf080eff4332b6b49390a2f365949db1584c275
Mango <meyoo9755@gmail.com>
2021-01-03 23:56:46 +0900

    #10.1 Permissions

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  ActivityIndicator,
+} from "react-native";
+import { Camera, Permissions } from "expo-camera";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [types, setTypes] = useState(null);
+  useEffect(() => {
+    (async () => {
+      const types = await Camera.getAvailableCameraTypesAsync();
+      alert(JSON.stringify(types));
+      setTypes(types);
+      if (Platform.OS === "web") {
+        setHasPermission(true);
+      } else {
+        const { status } = await Camera.requestPermissionsAsync();
+        setHasPermission(status === "granted");
+      }
+    })();
+  }, []);
+
+  if (hasPermission === true) {
+    return (
+      <View>
+        <Text>Has permissions</Text>
+      </View>
+    );
+  } else if (hasPermission === false) {
+    return (
+      <View>
+        <Text>Don't have permission for this</Text>
+      </View>
+    );
+  } else {
+    return <ActivityIndicator />;
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
